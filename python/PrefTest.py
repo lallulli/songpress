aa8866da5530570143f938c8b83d34aafbe3f182
lallulli <lallulli@d2329424-b1e2-11de-a2d3-9dcf383ffee4>
2009-06-02 10:56:22 +0000

    * Preferences: implemented init values

--- python/PrefTest.py
+++ python/PrefTest.py
@@ -9,12 +9,22 @@
 
 from Pref import *
 
+def EtaValidator(eta):
+	if eta < 0:
+		return True, 0
+	elif eta < 120:
+		return True, eta
+	else:
+		return False, 0
 
 class Persona(Preferences):
 	def __init__(self, parents = [], gui = None):
 		Preferences.__init__(self, parents, gui)	
 	
 Persona.Register("nome", str)
+Persona.Register("cognome", str, lambda: "Allulli")
+Persona.Register("eta", int, None, EtaValidator)
+
 #Register("figlio", Persona)
 """	
 a = Persona()
