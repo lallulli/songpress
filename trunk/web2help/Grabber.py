8f72fd2abd88116c2f23e7637de72277bc21c8c6
l.allulli <l.allulli@d2329424-b1e2-11de-a2d3-9dcf383ffee4>
2009-11-04 11:42:59 +0000

    * web2help: Navigation buttons refer to tree traversal order

--- trunk/web2help/Grabber.py
+++ trunk/web2help/Grabber.py
@@ -91,49 +91,54 @@ class Grabber(threading.Thread):
 		if load:
 			self.Load(url)
 	
-	def InsertIntoRepository(self, item):
-		t, u = glb.Split(self.tree.GetItemText(item))
-		self.repo.SetItemWithExt(u, '.htm')
-		self.DoWithChildren(item, self.InsertIntoRepository)
+	def InsertIntoRepository(self):
+		for k in self.treeList:
+			self.repo.SetItemWithExt(k[1], '.htm')
 		
-	def GetItemUrl(self, item):
-		if item.IsOk() and item != self.tree.GetRootItem():
-			dummy, url = glb.Split(self.tree.GetItemText(item))
-		else:
-			url = False
-		return url
+	def GetPrevItem(self, item):
+		prev = self.tree.GetPrevItem(item)
+		if item.IsOk():
+			return prev.GetItemUrl()
+		parent = self.tree.GetItemParent(item)
+		if parent != self.tree.GetRootItem():
+			prev = self.tree.GetNextItem(item)
+			if item.IsOk():
+				return next.GetItemUrl()
+		return False		
 		
-	def Grab(self, item):
-		# grab page and extract content
-		t, u = glb.Split(self.tree.GetItemText(item))
-		self.Send("Grabbing %s" % (u,))
-		s = urllib2.urlopen(u)
-		raw = s.read()
-		s.close()
-		soup = BeautifulSoup.BeautifulSoup(raw)
-		title = self.extractTitle(soup.html)
-		self.tree.SetItemText(item, glb.Join(title, u))
-		content = self.extractContent(soup.html)
-		if type(content) != list and type(content) != BeautifulSoup.ResultSet:
-			content = [content]
-		c = "".join([str(x) for x in content])
-		next = self.GetItemUrl(self.tree.GetNextSibling(item))
-		prev = self.GetItemUrl(self.tree.GetPrevSibling(item))
-		parent = self.GetItemUrl(self.tree.GetItemParent(item))
+	def Grab(self):
+		i = 0
+		nn = len(self.treeList)
+		for k in self.treeList:
+			# grab page and extract content
+			u = k[1]
+			self.Send("Grabbing %s" % (u,))
+			s = urllib2.urlopen(u)
+			raw = s.read()
+			s.close()
+			soup = BeautifulSoup.BeautifulSoup(raw)
+			title = self.extractTitle(soup.html)
+			self.tree.SetItemText(k[0], glb.Join(title, u))
+			content = self.extractContent(soup.html)
+			if type(content) != list and type(content) != BeautifulSoup.ResultSet:
+				content = [content]
+			c = "".join([str(x) for x in content])
+			next = self.treeList[i + 1][1] if i + 1 < nn else False
+			prev = self.treeList[i - 1][1] if i - 1 >= 0 else False
+			parent = k[2] if k[2] != "" else False
+			# apply template
+			text = self.tpl.generate(title=title, content=c, next=next, prev=prev, parent=parent).render('html')
+			# re-parse generated html, and "normalize" urls
+			soup = BeautifulSoup.BeautifulSoup(text)
+			a = soup.findAll('a')
+			img = soup.findAll('img')
+			for el in a:
+				self.TransformUrl(u, el, 'href')
+			for el in img:
+				self.TransformUrl(u, el, 'src', True)
+			self.Load(u, soup.renderContents())
+			i += 1
 
-		# apply template
-		text = self.tpl.generate(title=t, content=c, next=next, prev=prev, parent=parent).render('html')
-		# re-parse generated html, and "normalize" urls
-		soup = BeautifulSoup.BeautifulSoup(text)
-		a = soup.findAll('a')
-		img = soup.findAll('img')
-		for el in a:
-			self.TransformUrl(u, el, 'href')
-		for el in img:
-			self.TransformUrl(u, el, 'src', True)
-		self.Load(u, soup.renderContents())		
-		# recurse on children
-		self.DoWithChildren(item, self.Grab)
 		
 	def GenerateTocItem(self, item, e):
 		t, u = glb.Split(self.tree.GetItemText(item))
@@ -149,14 +154,19 @@ class Grabber(threading.Thread):
 				item,
 				lambda i: self.GenerateTocItem(i, ul)
 			)
-
-	def GenerateProjectItem(self, item, f):
+			
+	def GenerateTreeList(self, item):
 		t, u = glb.Split(self.tree.GetItemText(item))
-		f.write(self.repo[u] + "\n")
+		p = self.tree.GetItemParent(item)
+		if p != self.tree.GetRootItem():
+			pt, pu = glb.Split(self.tree.GetItemText(p))
+		else:
+			pu = ""
+		self.treeList.append((item, u, pu))
 		if self.tree.ItemHasChildren(item):
 			self.DoWithChildren(
 				item,
-				lambda i: self.GenerateProjectItem(i, f)
+				self.GenerateTreeList
 			)
 
 	def DoWithChildren(self, item, what):
@@ -209,10 +219,8 @@ Contents file=toc.hhc
 			self.project.name
 		)
 		f.write(i)
-		self.DoWithChildren(
-			self.tree.GetRootItem(),
-			lambda i: self.GenerateProjectItem(i, f)
-		)
+		for k in self.treeList:
+			f.write(self.repo[k[1]] + "\n")
 		f.close()		
 		
 	def Compile(self):
@@ -238,12 +246,16 @@ Contents file=toc.hhc
 			loader = TemplateLoader('.')
 			self.tpl = loader.load(self.project.template)
 			self.tpldir, f = os.path.split(self.project.template)
+						
+			# generate tree list
+			self.treeList = []
+			self.DoWithChildren(self.tree.GetRootItem(), self.GenerateTreeList)
 			
-			# traverse tree and insert urls into repository
-			self.DoWithChildren(self.tree.GetRootItem(), self.InsertIntoRepository)
+			# insert urls into repository
+			self.InsertIntoRepository()
 			
-			# traverse tree and get documents
-			self.DoWithChildren(self.tree.GetRootItem(), self.Grab)
+			# grab documents
+			self.Grab()
 			
 			# generate project files
 			self.GenerateToc()
@@ -268,8 +280,8 @@ Contents file=toc.hhc
 					os.rmdir(os.path.join(root, name))
 			"""
 		except Exception, ex:
-			#msg = "\nERROR:\n" + traceback.format_exc()
-			msg = "\nERROR:\n" + str(ex)
+			msg = "\nERROR:\n" + traceback.format_exc()
+			#msg = "\nERROR:\n" + str(ex)
 			self.Send(msg)
 	
 	def Stop(self):
