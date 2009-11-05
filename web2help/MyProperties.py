80d29be4082fd91c5f3ba5e4271593f70ebf6e8c
l.allulli <l.allulli@d2329424-b1e2-11de-a2d3-9dcf383ffee4>
2009-11-05 09:43:08 +0000

    * web2help: used STC to edit content and title extractors

--- web2help/MyProperties.py
+++ web2help/MyProperties.py
@@ -13,19 +13,20 @@ from Project import *
 class MyProperties(Properties):
 	def __init__(self, parent, project):
 		Properties.__init__(self, parent)
+		
 		self.project = project
 		self.template.SetPath(project.template)
 		self.css.SetPath(project.css)
-		self.title.SetValue(project.extractTitle)
-		self.content.SetValue(project.extractContent)
-		self.name.SetPath(project.name)
+		self.title.SetText(project.extractTitle)
+		self.content.SetText(project.extractContent)
+		self.name.SetPath(project.name)	
 	
 	def OnOk(self, evt):
 		p = self.project
 		p.template = self.template.GetPath()
 		p.css = self.css.GetPath()
-		p.extractTitle = self.title.GetValue()
-		p.extractContent = self.content.GetValue()
+		p.extractTitle = self.title.GetText()
+		p.extractContent = self.content.GetText()
 		p.name = self.name.GetPath()
 		evt.Skip()
 		
