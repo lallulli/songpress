372f20684e48721cf803547c41da4ddb0ef4fd5b
Axel Schneider <axel99092@t-online.de>
2020-12-16 09:27:08 +0100

    Update main.py
    changed import

--- src/main.py
+++ src/main.py
@@ -1,40 +1,39 @@
 ###############################################################
-# Name:			 main.py
-# Purpose:	 Entry point for Songpress
-# Author:		 Luca Allulli (webmaster@roma21.it)
-# Created:	 2009-01-16
+# Name:             main.py
+# Purpose:     Entry point for Songpress
+# Author:         Luca Allulli (webmaster@roma21.it)
+# Created:     2009-01-16
 # Copyright: Luca Allulli (https://www.skeed.it/songpress)
-# License:	 GNU GPL v2
+# License:     GNU GPL v2
 ##############################################################
 
-from SongpressFrame import *
+import sys, os
+import SongpressFrame
 import wx
-from wx import xrc
 from Globals import glb
 import i18n
 import dev_tool
 
 class SongpressApp(wx.App):
 
-	def OnInit(self):
-		self.SetAppName("songpress")
-		glb.InitDataPath()
-		self.config = wx.FileConfig(localFilename=os.path.join(glb.data_path, "config.ini"))
-		wx.Config.Set(self.config)
-		i18n.init(glb.default_language, [l for l in glb.languages])
-		self.config.SetPath("/App")
-		l = self.config.Read("locale")
-		if l:
-			i18n.setLang(l)
-		else:
-			i18n.setSystemLang()
-		self.res = xrc.XmlResource(glb.AddPath("xrc/songpress.xrc"))
-		songpressFrame = SongpressFrame(self.res)
-
-		return True
+    def OnInit(self):
+        self.SetAppName("songpress")
+        glb.InitDataPath()
+        self.config = wx.FileConfig(localFilename=os.path.join(glb.data_path, "config.ini"))
+        wx.Config.Set(self.config)
+        i18n.init(glb.default_language, [l for l in glb.languages])
+        self.config.SetPath("/App")
+        l = self.config.Read("locale")
+        if l:
+            i18n.setLang(l)
+        else:
+            i18n.setSystemLang()
+        self.res = wx.xrc.XmlResource(glb.AddPath("xrc/songpress.xrc"))
+        songpressFrame = SongpressFrame.SongpressFrame(self.res)
+        return True
 
 
 if __name__ == '__main__':
-	sys.excepthook = dev_tool.ExceptionHook
-	songpressApp = SongpressApp()
-	songpressApp.MainLoop()
+    sys.excepthook = dev_tool.ExceptionHook
+    songpressApp = SongpressApp()
+    songpressApp.MainLoop()
