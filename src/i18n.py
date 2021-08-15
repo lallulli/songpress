ba7a5f9323f6fd977c3a24b7a05192513d018229
Luca Allulli <luca@skeed.it>
2021-08-15 11:53:39 +0200

    i18n refactoring: using better global variables

--- src/i18n.py
+++ src/i18n.py
@@ -15,40 +15,56 @@ import locale
 import sys
 from Globals import *
 
-reg = []
-lang = ['']
-mylocale = [None]
-defaultLang = ['']
-supportedLangs = [[]]
+
+registered_modules = []
+# 2-character language code of current language
+current_language = None
+# wxPython wx.Locale object for current language
+mylocale = None
+# 2-character language code of default language, set during init
+defaultLang = None
+# Array mapping 2-character language codes of languages supported by Songpress
+# to language names. To be set during init phase, by calling init function
+supportedLangs = None
+
 
 def GetDomainAndLocale(name):
 	d, f = os.path.split(name.replace('.', '/'))
 	return (f, os.path.join(d, "locale"))
 
 def init(default_lang, supported_langs):
-	defaultLang[0] = default_lang
-	supportedLangs[0] = supported_langs
+	global defaultLang
+	global supportedLangs
+	defaultLang = default_lang
+	supportedLangs = supported_langs
+
 
 _ = lambda x: x
 
+
 def setLang(l):
-	lang[0] = l
-	for mod in reg:
-		if l != defaultLang[0]:
-			d, loc = GetDomainAndLocale(mod.__name__)
-			mod._ = gettext.translation(d, glb.AddPath(loc), languages=lang, codeset="iso-8859-1").gettext
-		else:
-			mod._ = gettext.NullTranslations().gettext
+	global current_language
+	current_language = l
+	for mod in registered_modules:
+		mod._ = wx.GetTranslation
+		# if l != defaultLang[0]:
+		# 	d, loc = GetDomainAndLocale(mod.__name__)
+		# 	mod._ = gettext.translation(d, glb.AddPath(loc), languages=lang, codeset="iso-8859-1").gettext
+		# else:
+		# 	mod._ = gettext.NullTranslations().gettext
+
 
 def getLang():
-	return lang[0]
-			
+	return current_language
+
+
 def setSystemLang():
 	l = locale.getdefaultlocale()
-	if l is not None and l[0][:2] in supportedLangs[0]:
+	if l is not None and l[0][:2] in supportedLangs:
 		setLang(l[0][:2])
 	else:
-		setLang(defaultLang[0])
+		setLang(defaultLang)
+
 
 def register(moduleName=None):
 	if moduleName is None:
@@ -59,20 +75,21 @@ def register(moduleName=None):
 	else:
 		mod = sys.modules[moduleName]
 	d, loc = GetDomainAndLocale(moduleName)
-	reg.append(mod)
-	if lang[0] == defaultLang[0]:
+	registered_modules.append(mod)
+	if current_language == defaultLang:
 		mod._ = gettext.NullTranslations().gettext
 	else:
-		mod._ = gettext.translation(d, glb.AddPath(loc), languages=lang, codeset="iso-8859-1").gettext
+		mod._ = gettext.translation(d, glb.AddPath(loc), languages=current_language, codeset="iso-8859-1").gettext
 
 
 def localizeXrc(filename):
 	import wx
-	if lang[0] != defaultLang[0]:
-		langid = wx.Locale.FindLanguageInfo(lang[0]).Language		
+	if current_language != defaultLang:
+		langid = wx.Locale.FindLanguageInfo(current_language).Language
 		d, domain = os.path.split(filename)
 		localedir = os.path.join(glb.AddPath(d), "locale")
 		# Set locale for wxWidgets
-		mylocale[0] = wx.Locale(langid)
-		mylocale[0].AddCatalogLookupPathPrefix(localedir)
-		mylocale[0].AddCatalog(domain)
+		global mylocale
+		mylocale = wx.Locale(langid)
+		mylocale.AddCatalogLookupPathPrefix(localedir)
+		mylocale.AddCatalog(domain)
